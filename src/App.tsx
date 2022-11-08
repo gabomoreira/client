fb1f1d7df2220b6794610fb702f4dfa976ff8e15
Gabriel Moreira <gsm.br2003@gmail.com>
2022-11-08 12:26:17 -0300

    feat: Adicionando menu lateral permanente

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { AppRoutes } from "./routes";
 import { BrowserRouter } from "react-router-dom";
 import { AppThemeProvider } from "./shared/contexts";
+import { MenuLateral } from "./shared/components";
 
 export const App = () => {
   return (
     <AppThemeProvider>
       <BrowserRouter>
-        <AppRoutes />
+        <MenuLateral>
+          <AppRoutes />
+        </MenuLateral>
       </BrowserRouter>
     </AppThemeProvider>
   );
