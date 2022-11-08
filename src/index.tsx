f1f36d5c00116b6134c728666ec95cbe39f67074
Gabriel Moreira <gsm.br2003@gmail.com>
2022-11-08 01:36:32 -0300

    feat: Adicionando tema claro e provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@mui/material";
+import { LightTheme } from "./shared/themes";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/shared/themes/Light.ts
@@ -0,0 +1,22 @@
+import { createTheme } from "@mui/material";
+import { cyan, green } from "@mui/material/colors";
+
+export const LightTheme = () =>
+  createTheme({
+    palette: {
+      primary: {
+        main: cyan[700],
+        dark: cyan[900],
+        light: cyan[500],
+      },
+      secondary: {
+        main: green[700],
+        dark: green[900],
+        light: green[500],
+      },
+      background: {
+        default: "#eee",
+        paper: "#fff",
+      },
+    },
+  });
