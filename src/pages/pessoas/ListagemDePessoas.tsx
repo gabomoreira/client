ab9ec2d918c8c1ac6a40fa3a5ed61f6c3aafb4af
Gabriel Moreira <gsm.br2003@gmail.com>
2022-11-11 14:01:04 -0300

    feat: Adicionado paginação em ListagemDePessoas

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -5,7 +5,19 @@ import { IListagemPessoa, PessoasService } from '../../shared/services/api/pesso
 import { FerramentasDaListagem } from '../../shared/components';
 import { useDebounce } from '../../shared/hooks';
 import { LayoutBaseDePagina } from '../../shared/layouts';
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import {
+  LinearProgress,
+  Pagination,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableFooter,
+  TableHead,
+  TableRow,
+} from '@mui/material';
+import { Environment } from '../../shared/environment';
 
 export const ListagemDePessoas: React.FC = () => {
   const [searchParams, setSerachParams] = useSearchParams();
@@ -18,22 +30,27 @@ export const ListagemDePessoas: React.FC = () => {
     return searchParams.get('busca') || '';
   }, [searchParams]);
 
+  const pagina = useMemo(() => {
+    return Number(searchParams.get('pagina') || '1');
+  }, [searchParams]);
+
   useEffect(() => {
     setIsLoading(true);
 
     debounce(() => {
-      PessoasService.getAll(1, busca).then((result) => {
+      PessoasService.getAll(pagina, busca).then((result) => {
         setIsLoading(false);
 
         if (result instanceof Error) {
           alert(result.message);
         } else {
+          console.log(result.data);
           setPessoas(result.data);
           setTotalCount(result.totalCount);
         }
       });
     });
-  }, [busca]);
+  }, [busca, pagina]);
 
   return (
     <LayoutBaseDePagina
@@ -43,34 +60,54 @@ export const ListagemDePessoas: React.FC = () => {
           textoBotaoNovo="Nova"
           mostrarInputBusca
           textoDaBusca={busca}
-          aoMudarTextoDeBusca={(texto) => setSerachParams({ busca: texto }, { replace: true })}
+          aoMudarTextoDeBusca={(texto) => setSerachParams({ busca: texto, pagina: '1' }, { replace: true })}
         />
       }
     >
       <TableContainer component={Paper} variant="outlined" sx={{ m: 1, width: 'auto' }}>
-        {isLoading ? (
-          'Carregando...'
-        ) : (
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Ações</TableCell>
-                <TableCell>Nome Completo</TableCell>
-                <TableCell>Email</TableCell>
-              </TableRow>
-            </TableHead>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Ações</TableCell>
+              <TableCell>Nome Completo</TableCell>
+              <TableCell>Email</TableCell>
+            </TableRow>
+          </TableHead>
 
-            <TableBody>
-              {pessoas?.map((pessoa) => (
-                <TableRow key={pessoa.id}>
-                  <TableCell>Ações</TableCell>
-                  <TableCell>{pessoa.nomeCompleto}</TableCell>
-                  <TableCell>{pessoa.email}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        )}
+          <TableBody>
+            {pessoas?.map((pessoa) => (
+              <TableRow key={pessoa.id}>
+                <TableCell>Ações</TableCell>
+                <TableCell>{pessoa.nomeCompleto}</TableCell>
+                <TableCell>{pessoa.email}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+
+          {totalCount === 0 && !isLoading && <caption>{Environment.LISTAGEM_VAZIA}</caption>}
+
+          <TableFooter>
+            {isLoading && (
+              <TableRow>
+                <TableCell colSpan={3}>
+                  <LinearProgress variant="indeterminate" />
+                </TableCell>
+              </TableRow>
+            )}
+
+            {totalCount != 0 && totalCount > Environment.LIMITE_DE_LINHAS && (
+              <TableRow>
+                <TableCell colSpan={3}>
+                  <Pagination
+                    page={pagina}
+                    onChange={(_, newPage) => setSerachParams({ busca, pagina: newPage.toString() }, { replace: true })}
+                    count={Math.ceil(totalCount / Environment.LIMITE_DE_LINHAS)}
+                  />
+                </TableCell>
+              </TableRow>
+            )}
+          </TableFooter>
+        </Table>
       </TableContainer>
     </LayoutBaseDePagina>
   );
