450ad2bf1733d3333517785e293ad7ccedda0df8
Gabriel Moreira <gsm.br2003@gmail.com>
2022-11-12 21:54:14 -0300

    feat: Adicionado Tradução das mensagens de erros do Yup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
-import { AppRoutes } from './routes';
 import { BrowserRouter } from 'react-router-dom';
+
+import './shared/forms/TraducoesYup';
+
+import { AppRoutes } from './routes';
 import { AppThemeProvider, DrawerProvider } from './shared/contexts';
 import { MenuLateral } from './shared/components';
 
